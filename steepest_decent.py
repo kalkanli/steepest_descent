6214f7695e3bd75aa6df9bb5d387b0d589f02c84
tahakalkanli <abdullahtahakalkanli@gmail.com>
2022-01-08 20:41:15 +0300

    overflow error but fuck it

--- steepest_decent.py
+++ steepest_decent.py
@@ -35,7 +35,7 @@ def steepest_decent(x1, x2):
     d = negative_transpose_gradient_function(x1, x2)
     table = [[x1,x2,function(x1, x2)]]
     while length(d) > eps and math.sqrt(x1*x1 + x2*x2) > eps:
-        alpha = minimize(x1, x2, d, 10, 0, 20)
+        alpha = minimize(x1, x2, d, 2.5, 0, 5)
         x1 = x1 + alpha*d[0]
         x2 = x2 + alpha*d[1]
         d = negative_transpose_gradient_function(x1, x2)
@@ -43,5 +43,5 @@ def steepest_decent(x1, x2):
     print(tabulate(table, headers=['x1', 'x2', 'f(x1, x2)']))
     return [x1, x2]
 
-steepest_decent(1, 5)
+steepest_decent(-2, 11)
 
